b9b9e2391a02e1565ab5c06efb9848618ed9de07
Oliwia Paliwoda <oliwia@LAPTOP-SGUVKNRU.>
2025-07-08 17:20:36 +0200

    page home

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,19 +1,50 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Home.scss';
 
 function Home() {
+
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpand = () => {
+        setExpanded(true);
+    };
+
+    const pageTtile = "CrystaLite";
+    const catchPhrase = "Your favorite crystal detection software...";
+    const getToKnow = "Click here to start your journey"
+
+    useEffect(() => {
+        if (expanded){
+            const timeout = setTimeout(() => {
+                const section = document.getElementById('section-to-scroll');
+                if (section) {
+                    section.scrollIntoView({behavior: 'smooth'});
+                }
+            }, 50);
+            return () => clearTimeout(timeout);
+        }
+    }, [expanded]);
+
     return(
         <div className="background">
-            <div className="amethyst"></div>
-            <div className="pagetitle">
-                CrystaLite
+
+            <img className="amethyst" src="/amethyst.png" alt="amethyst" />
+
+            <div className="stars-animation">
+                <div id='stars'></div>
+                <div id='stars2'></div>
+                <div id='stars3'></div>
+                <div id='stars4'></div>
             </div>
-            <div className="catchphrase">
-                Your favorite crystal detection software...
+
+
+            <div className="top">
+                <div className="pagetitle">{pageTtile}</div>
+            <div className="catchphrase">{catchPhrase}</div>
+            <div className="getToKnow">{getToKnow}</div>
             </div>
-            <div className="getToKnow">
-                Click here to start your journey.
-            </div>
+
+
 
         </div>
     );
